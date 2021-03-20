b83c0a1ddbb28ea68015f6d2e82d80d0119e5087
Ruslan Bekshenev <rbekshenev23@gmail.com>
2021-03-21 02:46:05 +0500

    Module HTTP

--- index.js
+++ index.js
@@ -1,7 +1,28 @@
-const obj = require('./user')
+// const obj = require('./user')
 
-console.log('Hello', __dirname)
-console.log('Hello', __filename)
-console.log('user: ', obj)
+// console.log('Hello', __dirname)
+// console.log('Hello', __filename)
+// console.log('user: ', obj)
 
-obj.sayHello()
+// obj.sayHello()
+
+// Создаем сервак =)
+const http = require('http')
+
+const server = http.createServer((req, res) => {
+  console.log(req.url)
+
+  res.write('<h1>Hello from NodeJS</h1>')
+  res.write('<h2>Hello from NodeJS</h2>')
+  res.write('<h3>Hello from NodeJS</h3>')
+  // конец ответа
+  res.end(`
+    <div style="background: red; width: 200px; height: 200px">
+      <h1>Test</h1>
+    </div>
+  `)
+})
+
+server.listen(3000, () => {
+  console.log('Server is runnig...')
+})

48f88692d9f2cd9383b432aaf86a3017decd4d1f
Ruslan Bekshenev <rbekshenev23@gmail.com>
2021-03-21 02:30:37 +0500

    events

---
+++ refs/event_ref.js
@@ -0,0 +1,16 @@
+// Module Event
+const EventEmmiter = require('events')
+
+class Logger extends EventEmmiter {
+  log(message) {
+    this.emit('message', `${message} ${Date.now()}`)
+  }
+}
+
+const logger = new Logger()
+
+logger.on('message', data => {
+  console.log(data)
+})
+
+logger.log('Hello')

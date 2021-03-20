fe04518069eb665fc2155675a8a6a5a8f8275d13
Ruslan Bekshenev <rbekshenev23@gmail.com>
2021-03-21 04:14:32 +0500

    условно это REST

--- index.js
+++ index.js
@@ -29,6 +29,23 @@ const server = http.createServer((req, res) => {
           res.end(content)
         }
       )
+    } else if (req.url === '/api/users') {
+      res.writeHead(200, {
+        'Content-Type': 'text/json'
+      })
+
+      const users = [
+        {
+          name: 'Vladilen', 
+          age: 25
+        }, 
+        {
+          name: 'Elena',
+          age: 23
+        }
+      ]
+
+      res.end(JSON.stringify(users))
     }
 
   } else if (req.method === 'POST') {
